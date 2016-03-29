5b4f043f058f8bc9ccb0ffabaa8e4bc814d38262
Louis Pipkin <louispipkin@Louiss-MacBook-Air.local>
2016-03-29 10:03:29 -0400

    emacs setup  version 3.1

---
+++ customizations/setup-slime.el
@@ -0,0 +1,5 @@
+;; Set your lisp system and, optionally, some contribs
+  ;;(setq inferior-lisp-program "/usr/local/bin/sbcl")
+  (setq inferior-lisp-program "/usr/local/bin/clisp -q -I")
+    (setq slime-contribs '(slime-fancy))
+
---
+++ themes/cast-iron-theme.el
@@ -0,0 +1,9 @@
+;; this is a custonm theme
+;; cast-iron
+;; adapting from green phosphor
+;;
+;;   (load-theme 'cast-iron t)
+;;
+;;; code
+
+(deftheme cast-iron)
